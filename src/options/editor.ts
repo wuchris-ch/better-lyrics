f517238dcfe68b6289523db65b7f80b61828ba22
Adalie Ahuja <hi@adalie.me>
2025-10-21 02:17:36 -0400

    Lint

--- src/options/editor.ts
+++ src/options/editor.ts
@@ -1,7 +1,7 @@
 import { EditorState } from "@codemirror/state";
-import { basicSetup } from '@codemirror/basic-setup';
+import { basicSetup } from "@codemirror/basic-setup";
 import { openSearchPanel, highlightSelectionMatches } from "@codemirror/search";
-import {type Diagnostic, linter, lintGutter, lintKeymap} from "@codemirror/lint";
+import { type Diagnostic, linter, lintGutter, lintKeymap } from "@codemirror/lint";
 import { indentWithTab, history, defaultKeymap, historyKeymap } from "@codemirror/commands";
 import {
   foldGutter,
@@ -25,7 +25,8 @@ import {
   keymap,
   EditorView,
   type EditorViewConfig,
-  ViewPlugin, tooltips,
+  ViewPlugin,
+  tooltips,
 } from "@codemirror/view";
 
 import { oneDark } from "@codemirror/theme-one-dark";
@@ -95,51 +96,51 @@ document.getElementById("back-btn")?.addEventListener("click", openOptions);
 const CONFIG = {
   rules: {
     // https://github.com/stylelint/stylelint-config-recommended/blob/main/index.js
-    'annotation-no-unknown': true,
-    'at-rule-no-unknown': true,
-    'block-no-empty': true,
-    'color-no-invalid-hex': true,
-    'comment-no-empty': true,
-    'custom-property-no-missing-var-function': true,
-    'declaration-block-no-duplicate-custom-properties': true,
-    'declaration-block-no-duplicate-properties': [
+    "annotation-no-unknown": true,
+    "at-rule-no-unknown": true,
+    "block-no-empty": true,
+    "color-no-invalid-hex": true,
+    "comment-no-empty": true,
+    "custom-property-no-missing-var-function": true,
+    "declaration-block-no-duplicate-custom-properties": true,
+    "declaration-block-no-duplicate-properties": [
       true,
       {
-        ignore: ['consecutive-duplicates-with-different-values'],
+        ignore: ["consecutive-duplicates-with-different-values"],
       },
     ],
-    'declaration-block-no-shorthand-property-overrides': true,
-    'font-family-no-duplicate-names': true,
-    'font-family-no-missing-generic-family-keyword': true,
-    'function-calc-no-unspaced-operator': true,
-    'function-linear-gradient-no-nonstandard-direction': true,
-    'function-no-unknown': true,
-    'keyframe-block-no-duplicate-selectors': true,
-    'keyframe-declaration-no-important': true,
-    'media-feature-name-no-unknown': true,
-    'named-grid-areas-no-invalid': true,
-    'no-descending-specificity': true,
-    'no-duplicate-at-import-rules': true,
-    'no-duplicate-selectors': true,
-    'no-empty-source': true,
-    'no-invalid-double-slash-comments': true,
-    'no-invalid-position-at-import-rule': true,
-    'no-irregular-whitespace': true,
-    'property-no-unknown': true,
-    'selector-pseudo-class-no-unknown': true,
-    'selector-pseudo-element-no-unknown': true,
-    'selector-type-no-unknown': [
+    "declaration-block-no-shorthand-property-overrides": true,
+    "font-family-no-duplicate-names": true,
+    "font-family-no-missing-generic-family-keyword": true,
+    "function-calc-no-unspaced-operator": true,
+    "function-linear-gradient-no-nonstandard-direction": true,
+    "function-no-unknown": true,
+    "keyframe-block-no-duplicate-selectors": true,
+    "keyframe-declaration-no-important": true,
+    "media-feature-name-no-unknown": true,
+    "named-grid-areas-no-invalid": true,
+    "no-descending-specificity": true,
+    "no-duplicate-at-import-rules": true,
+    "no-duplicate-selectors": true,
+    "no-empty-source": true,
+    "no-invalid-double-slash-comments": true,
+    "no-invalid-position-at-import-rule": true,
+    "no-irregular-whitespace": true,
+    "property-no-unknown": true,
+    "selector-pseudo-class-no-unknown": true,
+    "selector-pseudo-element-no-unknown": true,
+    "selector-type-no-unknown": [
       true,
       {
-        ignore: ['custom-elements'],
+        ignore: ["custom-elements"],
       },
     ],
-    'string-no-newline': true,
-    'unit-no-unknown': true,
-  }
+    "string-no-newline": true,
+    "unit-no-unknown": true,
+  },
 };
 
-const cssLinter =linter(async (view) => {
+const cssLinter = linter(async view => {
   let diagnostics: Diagnostic[] = [];
   const doc = view.state.doc.toString();
 
@@ -148,17 +149,21 @@ const cssLinter =linter(async (view) => {
     const result = await window.stylelint.lint({
       code: doc,
       config: {
-        ...CONFIG
+        ...CONFIG,
       },
-       // Suppress console output from stylelint
+      // Suppress console output from stylelint
     });
 
     if (result.results && result.results.length > 0) {
-      result.results[0].warnings.forEach((warning:any) => {
+      result.results[0].warnings.forEach((warning: any) => {
         console.log(warning);
         diagnostics.push({
           from: view.state.doc.line(warning.line).from + warning.column - 1,
-          to: view.state.doc.line(warning.endLine || warning.line).from + warning.column + (warning.endColumn ? warning.endColumn - warning.column : 0) - 1,
+          to:
+            view.state.doc.line(warning.endLine || warning.line).from +
+            warning.column +
+            (warning.endColumn ? warning.endColumn - warning.column : 0) -
+            1,
           severity: warning.severity as "error" | "warning" | "info",
           message: warning.text,
         });
